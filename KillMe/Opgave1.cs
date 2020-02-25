6c7816f1294e223ea3ed6ff8bbb7f5227f29b60e
Geefy <42178995+Geefy@users.noreply.github.com>
2020-02-25 10:06:17 +0100

    Add project files.

---
+++ KillMe/Opgave1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Threadster
+{
+    class Opgave1
+    {
+        //static int count = 0;
+        //static object _lock = new object();
+        //static void Main(string[] args)
+        //{
+        //    Thread countUp = new Thread(CountUp);
+        //    Thread countDown = new Thread(CountDown);
+
+        //    countUp.Start();
+        //    countDown.Start();
+        //    while (true)
+        //    {
+
+        //        Console.WriteLine(count);
+        //        Thread.Sleep(500);
+        //    }
+        //}
+        //static void CountUp()
+        //{
+        //    while (true)
+        //    {
+
+
+
+        //        count++;
+        //        count++;
+
+        //        Thread.Sleep(1000);
+
+        //    }
+
+        //}
+
+        //static void CountDown()
+        //{
+        //    while (true)
+        //    {
+
+        //        lock (_lock)
+        //            count--;
+        //        Thread.Sleep(1000);
+        //    }
+        //}
+    }
+}
---
+++ KillMe/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Threadster
+{
+    class Program
+    {
+        static int count = 0;
+        static object _lock = new object();
+        static void Main(string[] args)
+        {
+            Thread hashTagWriter= new Thread(Write);
+            Thread starWriter = new Thread(Write);
+            starWriter.Start('*');
+            hashTagWriter.Start('#');
+            
+        }
+       
+
+        static void Write(object output)
+        {
+            char write = (char)output;
+            while (true)
+            {
+
+                lock (_lock)
+                {
+                    count += 60;
+                    for (int i = 0; i < 60; i++)
+                    {
+
+                        Console.Write(output);
+                    }
+
+                    Console.Write(count + "\n");
+                    Thread.Sleep(500);
+                }
+            }
+        }
+    }
+}
